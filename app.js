189c56fcdce6783635456393554328f2966f4159
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-17 14:12:00 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const myHeading = document.getElementById('myHeading');
+const myTextInput = document.getElementById('myTextInput');
+const myButton = document.getElementById('myButton');
+
+myButton.addEventListener('click', () => {
+  myHeading.style.color = myTextInput.value;
+});
